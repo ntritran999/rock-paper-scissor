e5ada6336089fc9405279d2e21b9b003c427275b
ntritran999 <81624097+ntritran999@users.noreply.github.com>
2021-04-02 10:25:02 +0700

    Create r_p_s.py

---
+++ r_p_s.py
@@ -0,0 +1,54 @@
+from random import randint
+
+
+def invalid_move():
+    if human_choice!="rock" and human_choice!="paper" and human_choice!="scissor": return True
+
+def win():
+    if human_choice=="rock"     and computer_choice=="scissor":   return True
+    if human_choice=="paper"    and computer_choice=="rock":      return True
+    if human_choice=="scissor"  and computer_choice=="paper":     return True
+
+def lose():
+    if human_choice=="rock"     and computer_choice=="paper":     return True
+    if human_choice=="paper"    and computer_choice=="scissor":   return True
+    if human_choice=="scissor"  and computer_choice=="rock":      return True
+
+def draw():
+    if human_choice==computer_choice:                             return True
+
+# Main loop
+while True: 
+    i = randint(1, 3)
+    computer_choice = ""
+    human_choice = input("Choose 'rock', 'paper' or 'scissor': ")
+    if i == 1: computer_choice += "rock"
+    if i == 2: computer_choice += "paper"
+    if i == 3: computer_choice += "scissor"
+    
+    if invalid_move(): 
+        print("Invalid move!")
+
+    else:
+        print(f"You chose: {human_choice}")
+        print(f"Computer chose: {computer_choice}")
+        
+
+    if win():
+        print("YOU WON!")
+        play_again = input("Do you want to play again? [y]/[n]: ")
+        if play_again == "n":
+            print("Thanks for playing!")
+            break
+    if lose():
+        print("YOU LOST!")
+        play_again = input("Do you want to play again? [y]/[n]: ")
+        if play_again == "n":
+            print("Thanks for playing!")
+            break     
+    if draw():
+        print("It's a draw!")
+        play_again = input("Do you want to play again? [y]/[n]: ")
+        if play_again == "n":
+            print("Thanks for playing!")
+            break
